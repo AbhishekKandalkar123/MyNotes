eea4bd89eb1bf03b969f08838d5926a967d69740
Abhishek Kandalkar <abhishekkandalkar1289@gmail.com>
2022-04-10 23:36:17 +0530

    initial commit

---
+++ lib/views/login_view.dart
@@ -0,0 +1,87 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  bool isPressed = false;
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Login')),
+        body: Column(
+          children: [
+            TextField(
+              controller: _email,
+              enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                  hintText: 'Enter your email here',
+                  prefixIcon: Icon(Icons.mail)),
+            ),
+            TextField(
+              controller: _password,
+              obscureText: isPressed ? false : true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration: InputDecoration(
+                  hintText: 'Enter your password here',
+                  suffix: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (!isPressed) {
+                            isPressed = true;
+                          } else {
+                            isPressed = false;
+                          }
+                        });
+                      },
+                      icon: isPressed
+                          ? const Icon(Icons.visibility)
+                          : const Icon(Icons.visibility_off)),
+                  prefixIcon: const Icon(Icons.key)),
+            ),
+            TextButton(
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
+                try{
+                  final userCredential = await FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                      email: email, password: password);
+                }on FirebaseAuthException catch(e){
+                  print(e.code);
+                }
+              },
+              child: const Text('Login'),
+            ),
+          ],
+        ));
+  }
+
+}
